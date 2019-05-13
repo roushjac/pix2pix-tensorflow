b665a2ad835b12022fa6867e6f06ad57fda0fe70
roushjac <roush.jake@gmail.com>
2019-05-13 16:51:44 -0400

    Add script to display .pb file in tensorboard, start file to convert graph into .tflite format - visualizing the graph in tensorboard will help make this

---
+++ convert_graph_to_tflite.py
@@ -0,0 +1,7 @@
+import tensorflow as tf
+gf = tf.GraphDef()   
+m_file = open(r'models\edges2shoes_AtoB\graph.pb','rb')
+gf.ParseFromString(m_file.read())
+
+for node in gf.node:
+    print(node.name)
